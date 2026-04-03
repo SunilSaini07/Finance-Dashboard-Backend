1f09ccddad8b6a0fada072cce676ff3f6ff0995f
Sunil <sunilsaini98875@gmail.com>
2026-04-03 12:59:41 +0530

    Change the Access for the Viewer

--- src/main/java/com/example/finance_dashboard/controller/DashboardController.java
+++ src/main/java/com/example/finance_dashboard/controller/DashboardController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.http.HttpStatus;
+
 @RestController
 @RequestMapping("/summary")
 public class DashboardController {
@@ -17,11 +20,17 @@ public class DashboardController {
     }
 
     @GetMapping
-    public Map<String, Double> getSummary() {
+    public Map<String, Double> getSummary(@RequestHeader("Role") String role) {
+
+        if (!(role.equals("ADMIN") || role.equals("ANALYST"))) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access Denied");
+        }
+
         Map<String, Double> data = new HashMap<>();
         data.put("totalIncome", summaryService.getTotalIncome());
         data.put("totalExpense", summaryService.getTotalExpense());
         data.put("balance", summaryService.getBalance());
+
         return data;
     }
 }
