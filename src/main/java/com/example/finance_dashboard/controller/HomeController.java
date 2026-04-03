bba8633d9fbccb49eeb702bc31b3cc4f0f1dcc40
Sunil <sunilsaini98875@gmail.com>
2026-04-03 16:28:42 +0530

    Add controller for Home

---
+++ src/main/java/com/example/finance_dashboard/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.finance_dashboard.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Finance Dashboard API is running 🚀";
+    }
+}
